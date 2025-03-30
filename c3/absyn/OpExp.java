ee939afa0cccc573326b1a6c6cc5509fd90c67f9
jpalme08@uoguelph.ca <jpalme08@uoguelph.ca>
2025-03-30 17:51:12 -0400

    Updated OpExp with helper functions for C3

--- c3/absyn/OpExp.java
+++ c3/absyn/OpExp.java
@@ -44,4 +44,146 @@ public class OpExp extends Exp {
   public void accept( AbsynVisitor visitor, int level, boolean flag ) {
     visitor.visit( this, level, flag );
   }
+
+  //** from here are helps to help us with C3 **/
+  public String getOpName(){
+
+    String tmpStr = "";
+  
+    switch(op){
+
+      case 0:
+        tmpStr = "ADD";
+        break;
+      
+      case 1:
+        tmpStr = "SUB";
+        break;
+    
+      case 2:
+        tmpStr = "MUL";
+        break;
+    
+      case 3:
+        tmpStr = "DIV";
+        break;
+    
+      case 4:
+        tmpStr = "EQ";
+        break;
+    
+      case 5:
+        tmpStr = "LT";
+        break;
+
+      case 6:
+        tmpStr = "LTEQ";
+        break;
+    
+      case 7:
+        tmpStr = "GT";
+        break;
+    
+      case 8:
+        tmpStr = "GTEQ";
+        break;
+    
+      case 9:
+        tmpStr = "NEQ";
+        break;
+    
+      case 10:
+        tmpStr = "APPROX";
+        break;
+    
+      case 11:
+        tmpStr = "AND";
+        break;
+    
+      case 12:
+        tmpStr = "OR";
+        break;
+    
+      case 13:
+        tmpStr = "NOT";
+        break;
+    
+      case 14:
+        tmpStr = "UMINUS";
+        break;
+    
+    }
+    return tmpStr;
+  }
+
+  public String getOpSymbol(){
+    String tmpStr = "";
+  
+    switch(op){
+
+      case 0:
+        tmpStr = "+";
+        break;
+      
+      case 1:
+        tmpStr = "-";
+        break;
+    
+      case 2:
+        tmpStr = "*";
+        break;
+    
+      case 3:
+        tmpStr = "/";
+        break;
+    
+      case 4:
+        tmpStr = "==";
+        break;
+    
+      case 5:
+        tmpStr = "<";
+        break;
+
+      case 6:
+        tmpStr = "<=";
+        break;
+    
+      case 7:
+        tmpStr = ">";
+        break;
+    
+      case 8:
+        tmpStr = ">=";
+        break;
+    
+      case 9:
+        tmpStr = "!=";
+        break;
+    
+      case 10:
+        tmpStr = "~";
+        break;
+    
+      case 11:
+        tmpStr = "&&";
+        break;
+    
+      case 12:
+        tmpStr = "||";
+        break;
+    
+      case 13:
+        tmpStr = "!";
+        break;
+    
+      case 14:
+        tmpStr = "--"; //idk what this is supposed to be
+        break;
+    
+    }
+    return tmpStr;
+  }
+
+
 }
