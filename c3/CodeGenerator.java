69611b131cff4a8f344a95222ec0f66152259577
jpalme08@uoguelph.ca <jpalme08@uoguelph.ca>
2025-03-29 23:55:55 -0400

    Updated to have Absyn trees visit function, still need to do a little more for the prelude tho

--- c3/CodeGenerator.java
+++ c3/CodeGenerator.java
@@ -1,4 +1,5 @@
 import absyn.AbsynVisitor;
+import absyn.*;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Stack;
@@ -6,9 +7,12 @@ import java.util.Stack;
 public class CodeGenerator implements AbsynVisitor{
 
     /*Offsets */
+    public int mainEntry = -1; //updated with mains location if a main function is ever defined
     public int emitLoc = 0;
     public int highEmitLoc = 0;
     public int globalOffset = 0;
+    public int returnOffset = -1;
+    public int initialOffset = -2;
 
     /*Special Registers */
     public static final int PC = 7;
@@ -17,7 +21,7 @@ public class CodeGenerator implements AbsynVisitor{
     public static final int AC = 0;
     public static final int AC1 = 1;
 
-    /*Emit Functions */
+    /*Emit Functions given from the lectures*/
     void emitRO(String op, int r, int s, int t, String c) {
         System.out.printf("%3d: %5s %d, %d, %d \t%s\n", emitLoc, op, r, s, t, c);
         
@@ -72,4 +76,100 @@ public class CodeGenerator implements AbsynVisitor{
     }
     
     
+    
+    public void visit ( Absyn trees ) {
+
+        //prelude
+        emitComment("Standard Prelude:");
+        emitRM("LD", GP , 0,  AC , "Load gp with maxaddress");
+        emitRM("LDA", FP , 0,  GP , "Copy gp to fp");
+        emitRM("ST", AC , 0,  AC , "Clear value at location 0");
+
+        //inputs
+        emitComment("Jump around i/o routines here");
+        emitComment("code for input routine");
+        emitRM("ST", AC , returnOffset,  FP , "store return");
+        emitRO("IN", 0, 0, 0, "input");
+        emitRM("LD", PC , returnOffset,  FP , "return to caller");
+
+        //outputs
+        emitComment("code for output routine");
+        emitRM("ST", AC , returnOffset,  FP , "store return");
+        emitRM("LD", AC , initialOffset,  FP , "Load output value");
+        emitRO("OUT", 0, 0, 0, "output");
+        emitRM("LD", PC, returnOffset, FP, "return to caller");
+   
+
+        //Start generating the code based off the tree
+        trees.accept( this, 0, false );
+
+        //generate finale
+        emitRM( "ST", FP, -1, FP, "push ofp" );
+        emitRM( "LDA", FP, -1, FP, "push frame" );
+        emitRM( "LDA", AC, 1, PC, "load ac with ret ptr" );
+        emitRM_Abs( "LDA", PC, mainEntry, "jump to main loc" );
+        emitRM( "LD", FP, 0, FP, "pop frame" );
+        emitComment("End of execution.");
+        emitRO( "HALT", 0, 0, 0, "" );
+
+
+    }
+
+    //visit functions to red define again.... :| 
+
+    //here we call the declarations with true flag as dec list only occours in global scope according to grammar in cup files
+    public void visit( DecList decList, int offset, boolean flag ){
+
+        //loop through all declarations and pass true for isGlobal
+        while ( decList != null && decList.head != null) {
+            // VarDecList or Function Dec
+            decList.head.accept( this, offset, true );
+            offset--;
+            decList = decList.tail;
+        }
+
+    }
+
+    public void visit( ExpList exp, int offset, boolean flag );
+
+    public void visit( AssignExp exp, int offset, boolean flag );
+  
+    public void visit( IfExp exp, int offset, boolean flag );
+  
+    public void visit( IntExp exp, int offset, boolean flag );
+  
+    public void visit( OpExp exp, int offset, boolean flag );
+  
+    public void visit( WhileExp exp, int offset, boolean flag );
+  
+    public void visit( VarExp exp, int offset, boolean flag );
+  
+    public void visit( ArrayDec arrDec, int offset, boolean flag );
+  
+    public void visit( BoolExp exp , int offset, boolean flag );
+  
+    public void visit( CallExp exp, int offset, boolean flag );
+  
+    public void visit( CompoundExp exp, int offset, boolean flag );
+  
+    public void visit( FunctionDec FunDec, int offset, boolean flag );
+  
+    public void visit( IndexVar var, int offset, boolean flag );
+  
+    public void visit( NameTy type, int offset, boolean flag );
+  
+    public void visit( NilExp exp, int offset, boolean flag );
+  
+    public void visit( ReturnExp exp, int offset, boolean flag );
+  
+    public void visit( SimpleDec dec, int offset, boolean flag );
+  
+    public void visit( SimpleVar var, int offset, boolean flag );
+  
+    public void visit( VarDecList varDecList, int offset, boolean flag );
+
+
+
+
+
 }
